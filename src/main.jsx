eb56767bf9f5d8f608fd31e7767e33ccfe4655e8
krishnapriya E <e.krishnapriya5172@gmail.com>
2025-11-29 17:51:58 +0530

    Initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
+// main.jsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import { HashRouter } from 'react-router-dom'  // <-- import HashRouter
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <HashRouter>   {/* wrap App with HashRouter */}
+      <App />
+    </HashRouter>
+  </React.StrictMode>
 )
